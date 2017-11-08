5a5bd0ca18afc38168aa9b5b9e7d8502596f519c
Zach Spangler <zaspangler@gmail.com>
2017-11-08 11:06:22 -0700

    Final Javascript updates completed

--- index.php
+++ index.php
@@ -12,60 +12,71 @@
 	</head>
 	<body>
 		<!---NAVBAR--->
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-				</ul>
-				<form class="form-inline my-2 my-lg-0">
-					<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-				</form>
-			</div>
-		</nav>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand" href="#">All My Single Ladies</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+						  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Put</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Your</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Hands</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Up</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
 		<!---JUMBOTROM--->
 		<div class="jumbotron">
-			<h1 class="display-3">Hello, world!</h1>
-			<p class="lead">Meet Beyonce the Greatest Performer of All-Time. She is married to a man and rapper named Jay-Z. Together they make the world's greatest power couple.</p>
-			<p class="lead">
-				<a class="btn btn-primary btn-lg" href="#" role="button" id="click"
-					onclick="document.getElementById('click').innerHTML = 'They are the greatest';">
-					How great is this couple, click me to find out!</a>
-			</p>
+			<h1 class="display-3">Beyonce's, world!</h1>
+			<p class="lead">Meet Beyonce the Greatest Performer, Mom, Role Model & Friend of All-Time.</p>
+			<h6>Find out Beyonce's answer to any question, just click below</h6>
+			<a class="btn btn-primary btn-lg" href="#" role="button" id="click"
+				onclick="document.getElementById('click').innerHTML = 'Nickname: Queen B...nough said';">
+				How great is Beyonce?<br></a>
 		</div>
 		<!---IMAGES--->
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4 inline-block m1">
-					<img src="http://placebeyonce.com/450-300" alt="beyonce">
-					<button type="button" class="btn btn-secondary" id="mouse"
-							  onmouseover="document.getElementById('mouse').style.backgroundColor = 'red';">
-						Photo 1</button>
+				<div class="col-md-4 text-center mb-3">
+					<img src="http://placebeyonce.com/450-300" alt="beyonce" height="100%" width="100%" id="image2">
+					<button type="button" class="btn btn-secondary" id="mouse1"
+							  onmouseover="document.getElementById('mouse1').style.backgroundColor = 'red';"
+							  onclick="document.getElementById('image2').src='images/giphy-downsized-1.gif';">
+						See More
+					</button>
 				</div>
-				<div class="col-md-4 inline-block">
-					<img src="http://placebeyonce.com/450-300" alt="beyonce">
-					<button type="button" class="btn btn-secondary">Photo 1</button>
+				<div class="col-md-4 text-center mb-3">
+					<img src="http://placebeyonce.com/450-300" alt="beyonce" height="100%" width="100%" id="image3">
+					<button type="button" class="btn btn-secondary" id="mouse2"
+							  onmouseover="document.getElementById('mouse2').style.backgroundColor = 'blue';"
+							  onclick="document.getElementById('image3').src='images/giphy-downsized-2.gif';">
+						See More
+					</button>
 				</div>
-				<div class="col-md-4 inline-block">
-					<img src="http://placebeyonce.com/450-300" alt="beyonce">
-					<button type="button" class="btn btn-secondary">Photo 1</button>
+				<div class="col-md-4 text-center mb-3">
+					<img src="http://placebeyonce.com/450-300" alt="beyonce" height="100%" width="100%" id="image4">
+					<button type="button" class="btn btn-secondary" id="mouse3"
+							  onmouseover="document.getElementById('mouse3').style.backgroundColor = 'black';"
+							  onclick="document.getElementById('image4').src='images/giphy-downsized-3.gif';">
+						See More
+					</button>
 				</div>
 			</div>
 		</div>
 		<!---FOOTER--->
-		<footer>Skapa Development</footer>
+		<footer>Skapa Development &copy 2017</footer>
 		<!---JAVASCRIPT--->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
 				  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
